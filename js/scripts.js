17afbf5047011c4394c9dd6535a894db9d13c2fe
T-Dogg <47765355+T-Dogg-PO@users.noreply.github.com>
2021-04-22 17:37:37 +0200

    Added button for deleting item off list

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,28 @@ function newItem() {
         orderedList.append(listItem);
     }
 
-    // Event listener for crossing off an item when it's double clicked
-    listItem.on('dblclick', function() {
+    // Function for crossing off an itme when it's double clicked
+    function crossOffItem() {
         listItem.toggleClass('strike');
-    });
+    }
+
+    // Event listener for crossing off an item when it's double clicked
+    listItem.on('dblclick', crossOffItem);
+
+    // Function for deleting the item from the list when the X is clicked
+    function deleteListItem() {
+        listItem.addClass('delete');
+    }
+
+    // Add button for deleting an item from the list
+    let deleteButton = $('<crossOutButton></crossOutButton>');
+    deleteButton.append(document.createTextNode('X'));
+    listItem.append(deleteButton);
+
+    // Add event listener to call deleteListItem when the X is clicked
+    deleteButton.on('click', deleteListItem);
+
+
+
+
 }
